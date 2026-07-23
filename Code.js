bf6358b1079b17a5d6df8db317aeb5d12b87c276
tbintang <tbintang889@gmail.com>
2026-07-23 17:11:58 +0700

    Modul Sekolah

--- Code.js
+++ Code.js
@@ -91,6 +91,7 @@ function resolveTemplatePath(filename) {
     'JavascriptDashboard': 'views/scripts/modules/dashboard/JavascriptDashboard',
     'JavascriptLaporan': 'views/scripts/modules/laporan/JavascriptLaporan',
     // >>> Tambahkan mapping view entitas baru di sini <<<
+    'SekolahView': 'views/modules/SekolahView',
     'JurusanView': 'views/modules/JurusanView',
     'SiswaView': 'views/modules/SiswaView',
     'PembimbingView': 'views/modules/PembimbingView',
